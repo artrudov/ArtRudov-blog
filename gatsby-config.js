b967cce40046850e0e4aa91e70aa8ec72b65db73
Artem Rudov <artrudov@gmail.com>
2021-11-26 02:18:27 +0300

    set type script and eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,14 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/icon.png",
